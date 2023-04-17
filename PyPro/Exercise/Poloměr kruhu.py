6da63b28efc87c8edb7c4466247e58955d22af96
Gringo Gang <miroslav.pecek1@gmail.com>
2023-04-17 21:18:39 +0200

    exercises

---
+++ PyPro/Exercise/Poloměr kruhu.py
@@ -0,0 +1,12 @@
+print("Zadej poloměr kruhu (cm): ")
+
+input1 = float(input())
+pí = 3.14
+obvod = 2*input1*pí
+obsah =  pí*input1**2
+
+print("Obvod kruhu je:")
+print(obvod)
+
+print("Obsah kruhu je: ")
+print(obsah)
